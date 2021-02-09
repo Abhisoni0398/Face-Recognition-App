0b5767ce4f960c3e064052e954d66f44135af005
Abhishek Soni <57594416+Abhisoni0398@users.noreply.github.com>
2021-02-09 11:45:33 +0530

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://pure-journey-55851.herokuapp.com//signin', {
+    fetch('https://pure-journey-55851.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
